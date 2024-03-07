02e14688c921f46a32b2487f17b787871bd2ae86
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-03-07 17:29:42 -0400

    adds GPL3 notice to scripts

--- js/module.js
+++ js/module.js
@@ -75,3 +75,17 @@ export const clickEventHandler = async (e) => {
 		status.style['background-color'] = 'lightred'
 	}
 }
+
+/*
+
+FrontEnd Demo						March 7, 2024
+
+source: module.js
+author: @misael-diaz
+
+Copyright (c) 2024 Misael Díaz-Maldonado
+This file is released under the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+*/
