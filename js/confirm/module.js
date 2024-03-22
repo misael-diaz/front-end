3337cf88d3a52fc3015b732e61e2730fe54156df
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-03-21 20:37:06 -0400

    posts user credentials to REST API
    NOTE:
    this is when the REST API would store the user credentials
    in the database

--- js/confirm/module.js
+++ js/confirm/module.js
@@ -1,6 +1,6 @@
 export { clickEventHandler };
 
-const clickEventHandler = (e) => {
+const clickEventHandler = async (e) => {
 	e.preventDefault();
 	const code = document.getElementById('code').value;
 	const credentials = JSON.parse(window.sessionStorage.getItem("credentials"));
@@ -22,12 +22,38 @@ const clickEventHandler = (e) => {
 		return;
 	}
 
-	const msg = `successful account confirmation`;
-	const message = document.getElementById('message');
-	const errmsg = document.getElementById('error-message');
-	message.innerHTML = msg;
-	errmsg.style.opacity = 0;
-	console.log(msg);
+	const url = "http://localhost:8080/api/usr/confirm";
+	const opt = {
+		method: "POST",
+		mode: "cors",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(credentials)
+	};
+
+	try {
+		const response = await fetch(url, opt);
+		const json = await response.json();
+		console.log(json);
+		const msg = `successful account confirmation`;
+		const message = document.getElementById('message');
+		const errmsg = document.getElementById('error-message');
+		message.innerHTML = msg;
+		errmsg.style.opacity = 0;
+		console.log(msg);
+		setTimeout(() => {
+			window.location.replace('index.html');
+		}, 2000);
+		window.sessionStorage.removeItem("credentials");
+	} catch (err) {
+		console.error(`error: ${err}`);
+		const message = document.getElementById('message');
+		const errmsg = document.getElementById('error-message');
+		errmsg.innerHTML = `error: ${err}`;
+		errmsg.style.opacity = 1;
+		message.style.opacity = 0;
+	}
 };
 
 /*
